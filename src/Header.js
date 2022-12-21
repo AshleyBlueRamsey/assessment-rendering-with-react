538e12b445bc45ffb0f3763b43a144c3baeb945a
Ashley Ramsey <dusty1boy1984@gmail.com>
2022-12-21 15:16:01 -0500

    Wrote code for Header.js

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header({imageSrc, name, birthday}) {
+    return (
+        <header>
+            <img src ={imageSrc}></img>
+            <div>{name}</div>
+            <div>{birthday}</div>
+        </header>
+    );
+}
